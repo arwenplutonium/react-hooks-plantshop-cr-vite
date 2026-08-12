32a6bdf422d426fc4b9a8201dc7d632a2b9904b0
Daniella Achoki <daniellaachoki@gmail.com>
2026-08-13 01:24:39 +0300

    Add plant display feature

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard(props) {
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
+      <img src={props.plant.image} alt={props.plant.name} />
+      <h4>{props.plant.name}</h4>
+      <p>Price: {props.plant.price}</p>
       {true ? (
         <button className="primary">In Stock</button>
       ) : (
